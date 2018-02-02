ac745467ed4e8a2f09de3c67e2e0f067f78b9c41
福栄肥料株式会社 <>
2018-02-02 09:56:18 +0900

    seeds_destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: 'a@gmail.com', password: '000000', name: '小峯有貴')
+
