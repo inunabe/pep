16be288708925056be236ea4cf6e53a5455dd662
福栄肥料株式会社 <>
2018-02-02 14:06:06 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.create(email: "a@gmail.com", password: "000000", name:"小峯　有貴")
