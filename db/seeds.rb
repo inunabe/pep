c82643107c8dd481846172165903642127fd1134
福栄肥料株式会社 <>
2018-02-02 14:54:46 +0900

    seeds編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ require "csv"
 companies_csv = CSV.readlines("db/users.csv")
 companies_csv.shift
 companies_csv.each do |row|
-  Company.create(email: row[1], password: row[2], salt: row[3], name: row[4])
+  Company.create(email: row[1], crypted_password: row[2], salt: row[3], name: row[4])
 end
