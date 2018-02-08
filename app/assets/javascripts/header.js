cf79146f1e6b6e2569193fd2df2038bb7712dc9b
福栄肥料株式会社 <>
2018-02-08 18:36:32 +0900

    top_page背景

--- app/assets/javascripts/header.js
+++
@@ -1,4 +0,0 @@
-// $( ".dropdown-button" ).dropdown();
-// $('.container').on('dropdown', '.dropdown-button', function ());
-
-// $('.container').on('touchstart.dropdown.data-api', '.dropdown-menu', function (e) { e.stopPropagation() })
