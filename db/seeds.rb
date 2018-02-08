2d05a50431b3ca765c6335beb1ef541ce810f403
福栄肥料株式会社 <>
2018-02-08 21:56:54 +0900

    add csv

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+require "csv"
+question_alternatives_csv = CSV.readlines("db/question_alternatives.csv")
+question_alternatives_csv.shift
+question_alternatives_csv.each do |row|
+  Question_alternative.create(text: row[1], question_id: row[2])
+end
