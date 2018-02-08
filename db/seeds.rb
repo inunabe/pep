2a75462e10b01a4a52bcfabd693e76709edd7a1b
福栄肥料株式会社 <>
2018-02-08 21:01:31 +0900

    手直し

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ require "csv"
 question_alternatives_csv = CSV.readlines("db/question_alternatives.csv")
 question_alternatives_csv.shift
 question_alternatives_csv.each do |row|
-  Company.create(text: row[1], question_id: row[2])
+  Question_alternative.create(text: row[1], question_id: row[2])
 end
