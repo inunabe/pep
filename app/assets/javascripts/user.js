e5810dd205250b03d15eebba78c0b14e03dc846a
福栄肥料株式会社 <>
2018-02-15 21:09:31 +0900

    js_add_dropdown

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
+  $(".dropdown-button").dropdown();
   $('select').material_select();
+  $('select').material_select('destroy');
 });
-
-$('select').material_select('destroy');
