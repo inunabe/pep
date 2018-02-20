73cdb272497b53ccc1422608a477d007fbd4e222
福栄肥料株式会社 <>
2018-02-20 11:44:53 +0900

    API使って地図埋め込みテスト

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
   $('select').material_select();
 });
 
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('fukuei-map'), { // #sampleに地図を埋め込む
+        center: { // 地図の中心を指定
+            lat: 34.719735, // 緯度
+            lng: 135.418066 // 経度
+        },
+        zoom: 19 // 地図のズームを指定
+    });
+}
