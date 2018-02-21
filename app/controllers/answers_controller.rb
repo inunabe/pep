d269b5f453b2f93b5b24150d5e8bd7e01698bb16
福栄肥料株式会社 <>
2018-02-21 18:29:08 +0900

    評価編集準備

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -17,18 +17,17 @@ class AnswersController < ApplicationController
     redirect_to "/users/#{current_user.id}/subordinate_index",notice:"回答しました"
   end
 
-  # def edit
-  #   @questions = Question.all
-  #   answeres = Answer.where(answered_user_id: params[:user_id])
+  def edit
+    @questions = Question.all
+    @answers = Answer.where(answered_user_id: params[:user_id])
 
-  #   subordinate_answers_ids = []
-  #   answeres.each do |answer|
-  #     subordinate_answers_ids << answer.id
-  #   end
-  #   @subordinate_answers_ids = subordinate_answers_ids
-  #   @user = User.find(params[:user_id])
-  #   @answer = Answer.new(answer_user_id: Answer.find(answer_user.id))
-  #   binding.pry
-  # end
+    # subordinate_answers_ids = []
+    # answeres.each do |answer|
+    #   subordinate_answers_ids << answer.id
+    # end
+    # @subordinate_answers_ids = subordinate_answers_ids
+    # @user = User.find(params[:user_id])
+    # @answer = Answer.new(answer_user_id: Answer.find(answer_user.id))
+  end
 
 end
