84105b49f27bbdc9b0eefdd6b0bcff504e97c526
福栄肥料株式会社 <>
2018-02-28 12:26:35 +0900

    seetsファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,7 @@
-# require "csv"
-# question_alternatives_csv = CSV.readlines("db/question_alternatives.csv")
-# question_alternatives_csv.shift
-# question_alternatives_csv.each do |row|
-#   QuestionAlternative.create(text: row[1], question_id: row[2])
-
-
 equire "csv"
 departments_csv = CSV.readlines("db/departments.csv")
 departments_csv.shift
 departments_csv.each do |row|
   Department.create(name: row[1])
-
-# equire "csv"
-# grades_csv = CSV.readlines("db/grades.csv")
-# grades_csv.shift
-# grades_csv.each do |row|
-#   Grade.create(department_id: row[1],rank: row[2])
 
 end
