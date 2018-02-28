730c3d2db9398492e3e6302b2720cc84272faf79
福栄肥料株式会社 <>
2018-02-28 15:26:47 +0900

    中間テーブルに基づくモデルファイル編集

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,5 +1,5 @@
 class Grade < ActiveRecord::Base
   has_many :users
-  has_many :questions
+  has_many :questions, through: :grade_questions
   belongs_to :department
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   has_many :question_alternatives
   has_many :answers
   accepts_nested_attributes_for :question_alternatives
-  has_many :grades
+  has_many :grades, through: :grade_questions
 end
