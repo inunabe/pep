3482d6ce51c57635a403dedc52a7e0416072a1da
福栄肥料株式会社 <>
2018-03-07 15:59:53 +0900

    gradeによって表示する質問を変えた
    質問一覧で登録ずみのgrade表示
    gradeによって表示する質問を変えた

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,11 @@
 class AnswersController < ApplicationController
   def new
-    @questions = Question.all
+    @user = User.find(params[:user_id])
+    @questions = @user.grade.questions
+    @answer = Answer.new
+  end
+  def self_new
+    @questions = current_user.grade.questions
     @answer = Answer.new
     @user = User.find(params[:user_id])
   end
@@ -8,7 +13,6 @@ class AnswersController < ApplicationController
   def create
     questions = Question.all
     question_ids = questions.pluck(:id)
-
     rates = [params[:rate0],params[:rate1],params[:rate2],params[:rate3],params[:rate4],params[:rate5],params[:rate6],params[:rate7],params[:rate8],params[:rate9],params[:rate10],params[:rate11],params[:rate12],params[:rate13],params[:rate14]]
     question_ids_rates = question_ids.zip(rates)
     question_ids_rates.each do |question_id,rate|
@@ -21,17 +25,17 @@ class AnswersController < ApplicationController
     end
   end
 
-  def edit
-    @questions = Question.all
-    answers = Answer.where(answered_user_id: params[:user_id])
+  # def edit
+  #   @questions = Question.all
+  #   answers = Answer.where(answered_user_id: params[:user_id])
 
-    subordinate_answers_ids = []
-    answers.each do |answer|
-      subordinate_answers_ids << answer.id
-    end
-    @subordinate_answers_ids = subordinate_answers_ids
+  #   subordinate_answers_ids = []
+  #   answers.each do |answer|
+  #     subordinate_answers_ids << answer.id
+  #   end
+  #   @subordinate_answers_ids = subordinate_answers_ids
     # @user = User.find(params[:user_id])
     # @answer = Answer.new(answer_user_id: Answer.find(answer_user.id))
-  end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-
   validates :password, length: {minimum: 6}
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
