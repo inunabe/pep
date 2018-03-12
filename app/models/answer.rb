0d7e7578874344c3bb1a35bf0a951feaf466b7b2
福栄肥料株式会社 <>
2018-03-12 16:50:22 +0900

    periodsとanswersのアソシエーション組んだ

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :answering_user, class_name: 'User', foreign_key: :answering_user_id
   belongs_to :answered_user, class_name: 'User', foreign_key: :answered_user_id
+  belongs_to :period
 end
