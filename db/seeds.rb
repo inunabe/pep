581b415f223b26118b4ddca1acd61e1e0aff9ada
福栄肥料株式会社 <>
2018-03-19 18:58:19 +0900

    add_weight

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require "csv"
 questions_csv = CSV.readlines("db/questions.csv")
 questions_csv.shift
 questions_csv.each do |row|
-  Period.create(text: row[1],weight: row[4])
+  Question.create(text: row[1],weight: row[4])
 end
